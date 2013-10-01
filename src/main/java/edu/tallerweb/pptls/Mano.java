edf254a915d8c5b968c05f7563ee24b4408ea95f
bodhisattva <sergioadamoli@millanel.com>
2013-10-01 12:42:42 -0300

    Braid me las tenes por el sueeelooo ponemee 10 daleee

--- src/main/java/edu/tallerweb/pptls/Mano.java
+++ src/main/java/edu/tallerweb/pptls/Mano.java
@@ -7,61 +7,35 @@ package edu.tallerweb.pptls;
 
 public class Mano {
 
-
 	private Forma forma;
 	
 	private Resultado matriz[][];
 
-		public Mano(final Forma forma) {
-			
-			this.forma = forma;
-			
-			this.matriz = new Resultado[][] {
-												{ Resultado.EMPATA,	
-													Resultado.PIERDE,	
-													Resultado.PIERDE,	
-													Resultado.GANA,		
-													Resultado.GANA 		
-												},
-												{ Resultado.GANA,	
-												  Resultado.EMPATA,	
-												  Resultado.PIERDE,	
-												  Resultado.PIERDE,	
-												  Resultado.GANA 		
-												 },
-												{ Resultado.GANA,	
-												  Resultado.GANA,		
-												  Resultado.EMPATA,	
-												  Resultado.PIERDE,	
-												  Resultado.PIERDE 	
-												},
-												{ Resultado.PIERDE,	
-												  Resultado.GANA,		
-												  Resultado.GANA,		
-												  Resultado.EMPATA,	
-												  Resultado.PIERDE 	
-												},
-												{ Resultado.PIERDE,	
-												  Resultado.PIERDE,	
-												  Resultado.GANA,		
-												  Resultado.GANA,		
-												  Resultado.EMPATA 	
-												 } 
-											 };
-	
-		}
+	public Mano(final Forma forma) {
+		
+		this.forma = forma;
+		
+		this.matriz = new Resultado[][] {
+				{ Resultado.EMPATA,	Resultado.PIERDE,	Resultado.PIERDE,	Resultado.GANA,		Resultado.GANA },
+				{ Resultado.GANA,	Resultado.EMPATA,	Resultado.PIERDE,	Resultado.PIERDE,	Resultado.GANA },
+				{ Resultado.GANA,	Resultado.GANA,		Resultado.EMPATA,	Resultado.PIERDE,	Resultado.PIERDE },
+				{ Resultado.PIERDE,	Resultado.GANA,		Resultado.GANA,		Resultado.EMPATA,	Resultado.PIERDE },
+				{ Resultado.PIERDE,	Resultado.PIERDE,	Resultado.GANA,		Resultado.GANA,		Resultado.EMPATA } 
+			};
 
-		/**
-		 * Evaluará el resultado de la partida según las reglas
-		 * del juego.
-		 * @param otra, la otra Mano.
-		 * @return un Resultado, de acuerdo al estado del juego.
-		 */
-	
-		public Resultado jugarCon(final Mano otra) {
-	
-			return this.matriz[this.forma.getValor()][otra.forma.getValor()];
-	
-		}
+	}
+
+	/**
+	 * Evaluará el resultado de la partida según las reglas
+	 * del juego.
+	 * @param otra, la otra Mano.
+	 * @return un Resultado, de acuerdo al estado del juego.
+	 */
+
+	public Resultado jugarCon(final Mano otra) {
+
+		return this.matriz[this.forma.getValor()][otra.forma.getValor()];
+
+	}
 
 }
